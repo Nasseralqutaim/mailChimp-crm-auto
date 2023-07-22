b9e945e16bd2054ec1e6955e6c566f0ed5c45beb
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-07-22 13:06:37 -0700

    first commit & create EmailCampaigns component

---
+++ src/components/EmailCampaigns.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- ... -->
+
+  <!-- List of campaigns -->
+  <div v-for="campaign in campaigns" :key="campaign.id">
+    <!-- Campaign details will go here -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      // ...
+      campaigns: [],
+    };
+  },
+  methods: {
+    async fetchCampaigns() {
+      const response = await axios.get("/api/email-campaigns");
+      this.campaigns = response.data;
+    },
+    // ...
+  },
+  async mounted() {
+    await this.fetchCampaigns();
+  },
+};
+</script>
+
+<!-- ... -->
