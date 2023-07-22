c14b3eaa9ac66515f9f24653a2ff58069ed6986e
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-07-22 13:10:47 -0700

    add buttons to edit and delete each campaign

--- src/components/EmailCampaigns.vue
+++ src/components/EmailCampaigns.vue
@@ -3,7 +3,12 @@
 
   <!-- List of campaigns -->
   <div v-for="campaign in campaigns" :key="campaign.id">
-    <!-- Campaign details will go here -->
+    <h2>{{ campaign.name }}</h2>
+    <p>Scheduled Time: {{ campaign.scheduled_time }}</p>
+    <p>Target Segment: {{ campaign.target_segment }}</p>
+    <p>Email Body: {{ campaign.email_body }}</p>
+    <button @click="editCampaign(campaign)">Edit</button>
+    <button @click="deleteCampaign(campaign.id)">Delete</button>
   </div>
 </template>
 
