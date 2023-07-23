d1d81d7cbb93945992efc744b4dee92369a458fc
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-07-23 13:07:55 -0700

    add methods to EmailCampaings page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <EmailCampaigns />
+    <!-- other components and HTML here -->
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import EmailCampaigns from "./components/EmailCampaigns.vue";
 
-nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    EmailCampaigns,
+  },
+  // other options here
+};
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<!-- styles here -->
--- src/components/EmailCampaigns.vue
+++ src/components/EmailCampaigns.vue
@@ -2,6 +2,14 @@
   <!-- ... -->
 
   <!-- List of campaigns -->
+  <!-- Form for creating or updating a campaign -->
+  <form @submit.prevent="isUpdating ? updateCampaign : createCampaign">
+    <!-- ... -->
+    <button type="submit">{{ isUpdating ? "Update" : "Create" }} Campaign</button>
+    <!-- Button to cancel the update if we're updating -->
+    <button v-if="isUpdating" type="button" @click="cancelUpdate">Cancel</button>
+  </form>
+
   <div v-for="campaign in campaigns" :key="campaign.id">
     <h2>{{ campaign.name }}</h2>
     <p>Scheduled Time: {{ campaign.scheduled_time }}</p>
@@ -19,15 +27,60 @@ export default {
   data() {
     return {
       // ...
+      selectedCampaign: null,
       campaigns: [],
     };
   },
+  computed: {
+    isUpdating() {
+      return !!this.selectedCampaign;
+    },
+  },
   methods: {
+    async createCampaign() {
+      await axios.post("/api/email-campaigns", this.newCampaign);
+      await this.fetchCampaigns();
+      this.newCampaign = {
+        name: "",
+        scheduled_time: "",
+        target_segment: "",
+        email_body: "",
+      };
+    },
+    editCampaign(campaign) {
+      this.newCampaign = { ...campaign };
+      this.selectedCampaign = campaign;
+    },
+    async updateCampaign() {
+      await axios.put(`/api/email-campaigns/${this.selectedCampaign.id}`, this.newCampaign);
+      await this.fetchCampaigns();
+      this.newCampaign = {
+        name: "",
+        scheduled_time: "",
+        target_segment: "",
+        email_body: "",
+      };
+      this.selectedCampaign = null;
+    },
+    cancelUpdate() {
+      this.newCampaign = {
+        name: "",
+        scheduled_time: "",
+        target_segment: "",
+        email_body: "",
+      };
+      this.selectedCampaign = null;
+    },
+
     async fetchCampaigns() {
       const response = await axios.get("/api/email-campaigns");
       this.campaigns = response.data;
     },
-    // ...
+
+    async deleteCampaign(id) {
+      await axios.delete(`/api/email-campaigns/${id}`);
+      await this.fetchCampaigns();
+    },
   },
   async mounted() {
     await this.fetchCampaigns();
